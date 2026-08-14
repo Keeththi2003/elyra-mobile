426a2e91dedb93253d7c0612334cb952bf24695d
Keeththigan <keeththi2003@gmail.com>
2026-08-14 23:01:31 +0530

    feat: integrate AuthNavigation into ElyraApp component

--- app/src/main/java/com/keeththigan/elyra/app/ElyraApp.kt
+++ app/src/main/java/com/keeththigan/elyra/app/ElyraApp.kt
@@ -7,19 +7,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.keeththigan.elyra.core.designsystem.ElyraTheme
+import com.keeththigan.elyra.feature.auth.navigation.AuthNavigation
+
 
 @Composable
 fun ElyraApp() {
     ElyraTheme {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Elyra",
-                color = ElyraTheme.colors.textPrimary,
-                style = ElyraTheme.typography.headlineLarge
-            )
-        }
+        // Box(
+        //     modifier = Modifier.fillMaxSize(),
+        //     contentAlignment = Alignment.Center
+        // ) {
+        //     Text(
+        //         text = "Elyra",
+        //         color = ElyraTheme.colors.textPrimary,
+        //         style = ElyraTheme.typography.headlineLarge
+        //     )
+        // }
+        AuthNavigation()
     }
 }
