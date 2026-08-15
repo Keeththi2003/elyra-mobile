249837b935add140a7614da07dd592509122d1f7
Keeththigan <keeththi2003@gmail.com>
2026-08-15 11:52:42 +0530

    feat: add Room data model class

---
+++ app/src/main/java/com/keeththigan/elyra/data/model/Room.kt
@@ -0,0 +1,11 @@
+package com.keeththigan.elyra.data.model
+
+import com.google.firebase.Timestamp
+
+data class Room(
+    val id: String = "",
+    val name: String = "",
+    val floorId: String = "",
+    val createdAt: Timestamp? = null,
+    val updatedAt: Timestamp? = null
+)
