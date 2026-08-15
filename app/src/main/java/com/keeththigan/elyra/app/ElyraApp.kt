1dafeb9426c63eb5a72c0513c68194d57dc01566
Keeththigan <keeththi2003@gmail.com>
2026-08-15 13:22:18 +0530

    feat: enhance ElyraApp to integrate authentication handling with AuthViewModel

--- app/src/main/java/com/keeththigan/elyra/app/ElyraApp.kt
+++ app/src/main/java/com/keeththigan/elyra/app/ElyraApp.kt
@@ -1,31 +1,40 @@
 package com.keeththigan.elyra.app
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import com.keeththigan.elyra.core.designsystem.ElyraTheme
-import com.keeththigan.elyra.feature.auth.navigation.AuthNavigation
-import com.keeththigan.elyra.feature.home.presentation.HomeScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.keeththigan.elyra.app.navigation.AppNavigation
-
+import com.keeththigan.elyra.core.designsystem.ElyraTheme
+import com.keeththigan.elyra.data.repository.AuthRepository
+import com.keeththigan.elyra.feature.auth.AuthViewModel
+import com.keeththigan.elyra.feature.auth.AuthViewModelFactory
+import com.keeththigan.elyra.feature.auth.navigation.AuthNavigation
 
 @Composable
 fun ElyraApp() {
+
     ElyraTheme {
-        // Box(
-        //     modifier = Modifier.fillMaxSize(),
-        //     contentAlignment = Alignment.Center
-        // ) {
-        //     Text(
-        //         text = "Elyra",
-        //         color = ElyraTheme.colors.textPrimary,
-        //         style = ElyraTheme.typography.headlineLarge
-        //     )
-        // }
-//        AuthNavigation()
-           AppNavigation()
+
+        val authRepository = AuthRepository()
+
+        val authViewModel: AuthViewModel = viewModel(
+            factory = AuthViewModelFactory(
+                repository = authRepository
+            )
+        )
+
+        val authState =
+            authViewModel.authState.collectAsStateWithLifecycle()
+
+        if (authState.value.isAuthenticated) {
+
+            AppNavigation()
+
+        } else {
+
+            AuthNavigation(
+                authViewModel = authViewModel
+            )
+        }
     }
 }
